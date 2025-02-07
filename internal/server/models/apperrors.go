36966222cf68d3cc88f50cd940600cf708efd45f
mxtrap <makstrap.23.08@gmail.com>
2025-02-07 10:38:27 +0300

    try to refactor metrics service

--- internal/server/models/apperrors.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "errors"
-
-var (
-	ErrUnknownMetricType = errors.New("unknown metric type")
-	ErrNotFoundMetric    = errors.New("metric not found")
-	ErrWrongMetricValue  = errors.New("wrong metric value")
-)
--- internal/server/service/metrics_service.go
+++ internal/server/service/metrics_service.go
@@ -1,19 +1,27 @@
 package service
 
 import (
-	"errors"
-	"strconv"
 	"strings"
 
 	"github.com/MxTrap/metrics/internal/server/models"
 )
 
 type Storage interface {
-	SaveGaugeMetric(metric string, value float64)
-	SaveCounterMetric(metric string, value int64)
-	FindGaugeMetric(metric string) (float64, bool)
-	FindCounterMetric(metric string) (int64, bool)
+	gaugeMetricsStorage
+	counterMetricsStorage
 	GetAll() map[string]any
+}
+
+type metricTypes map[string]metricTypeService
+
+type metricTypeService interface {
+	Save(metric string, value string) error
+	Find(metric string) (any, bool)
+}
+
+func (t metricTypes) GetMetricTypeService(metricType string) (metricTypeService, bool) {
+	val, ok := t[metricType]
+	return val, ok
 }
 
 type MetricsService struct {
@@ -21,57 +29,13 @@ type MetricsService struct {
 	storage     Storage
 }
 
-type metricTypes map[string]metricTypeFunc
-
-type metricTypeFunc struct {
-	ParseFunc func(str string) (any, error)
-	SaveFunc  func(metric string, value any) error
-	FindFunc  func(metric string) (any, bool)
-}
-
-func (t metricTypes) GetMetricFunctions(metricType string) (metricTypeFunc, bool) {
-	val, ok := t[metricType]
-	return val, ok
-}
-
 func NewMetricsService(storage Storage) *MetricsService {
 
 	return &MetricsService{
 		storage: storage,
 		metricTypes: metricTypes{
-			"gauge": {
-				ParseFunc: func(str string) (any, error) {
-					return strconv.ParseFloat(str, 64)
-				},
-				SaveFunc: func(metric string, value any) error {
-					cVal, ok := value.(float64)
-					if !ok {
-						return errors.New("value is not a float64")
-					}
-					storage.SaveGaugeMetric(metric, cVal)
-					return nil
-				},
-				FindFunc: func(metric string) (any, bool) {
-					return storage.FindGaugeMetric(metric)
-				},
-			},
-
-			"counter": {
-				ParseFunc: func(str string) (any, error) {
-					return strconv.ParseInt(str, 10, 64)
-				},
-				SaveFunc: func(metric string, value any) error {
-					cVal, ok := value.(int64)
-					if !ok {
-						return errors.New("value is not a int64")
-					}
-					storage.SaveCounterMetric(metric, cVal)
-					return nil
-				},
-				FindFunc: func(metric string) (any, bool) {
-					return storage.FindCounterMetric(metric)
-				},
-			},
+			"gauge":   NewGaugeMetricService(storage),
+			"counter": NewCounterMetricService(storage),
 		},
 	}
 }
@@ -99,17 +63,12 @@ func (s *MetricsService) Save(url string) error {
 	}
 	metricType, metric, metricValue := parsedMetric[0], parsedMetric[1], parsedMetric[2]
 
-	acceptedMetricType, ok := s.metricTypes.GetMetricFunctions(metricType)
+	acceptedMetricType, ok := s.metricTypes.GetMetricTypeService(metricType)
 	if !ok {
 		return models.ErrUnknownMetricType
 	}
 
-	parsedValue, err := acceptedMetricType.ParseFunc(metricValue)
-	if err != nil || parsedValue == nil {
-		return models.ErrWrongMetricValue
-	}
-
-	err = acceptedMetricType.SaveFunc(metric, parsedValue)
+	err = acceptedMetricType.Save(metric, metricValue)
 	if err != nil {
 		return err
 	}
@@ -123,11 +82,11 @@ func (s *MetricsService) Find(url string) (any, error) {
 		return nil, err
 	}
 	metricType, metric := parsedMetric[0], parsedMetric[1]
-	metricFunc, ok := s.metricTypes.GetMetricFunctions(metricType)
+	metricFunc, ok := s.metricTypes.GetMetricTypeService(metricType)
 	if !ok {
 		return nil, models.ErrUnknownMetricType
 	}
-	val, ok := metricFunc.FindFunc(metric)
+	val, ok := metricFunc.Find(metric)
 	if !ok {
 		return nil, models.ErrNotFoundMetric
 	}
