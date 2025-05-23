7204e213d77a19b6df73164b1392c1fe9fa69357
mxtrap <makstrap.23.08@gmail.com>
2025-05-23 10:09:06 +0300

    add build flags

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/MxTrap/metrics/config"
 	"github.com/MxTrap/metrics/internal/agent/app"
 	"log"
@@ -12,7 +13,31 @@ import (
 	"syscall"
 )
 
+var (
+	BuildDate    string
+	BuildCommit  string
+	BuildVersion string
+)
+
+func formatFlagValue(val string) string {
+	if val == "" {
+		return "N/A"
+	}
+	return val
+}
+
+func printBuildFlags() {
+	fmt.Printf(
+		"Build version: %s\nBuild date: %s\nBuild commit: %s\n",
+		formatFlagValue(BuildVersion),
+		formatFlagValue(BuildDate),
+		formatFlagValue(BuildCommit),
+	)
+}
+
 func main() {
+	printBuildFlags()
+
 	cfg, err := config.NewAgentConfig()
 	ctx, cancel := context.WithCancel(context.Background())
 
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"github.com/MxTrap/metrics/config"
 	"github.com/MxTrap/metrics/internal/server/app"
 	"log"
 )
 
+var (
+	BuildDate    string
+	BuildCommit  string
+	BuildVersion string
+)
+
+func formatFlagValue(val string) string {
+	if val == "" {
+		return "N/A"
+	}
+	return val
+}
+
+func printBuildFlags() {
+	fmt.Printf(
+		"Build version: %s\nBuild date: %s\nBuild commit: %s\n",
+		formatFlagValue(BuildVersion),
+		formatFlagValue(BuildDate),
+		formatFlagValue(BuildCommit),
+	)
+}
+
 func main() {
+	printBuildFlags()
+
 	cfg, err := config.NewServerConfig()
 	if err != nil {
 		log.Fatal(err)
